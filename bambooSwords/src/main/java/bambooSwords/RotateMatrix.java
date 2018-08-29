e572c00c08bf143e26ca006d0c846e3b22c169e7
Sudaksh Chohan <sudakshc@oath.com>
2018-08-29 14:47:23 -0700

    New Thread examples, empty shells for stair case and rotating matirx problem

---
+++ bambooSwords/src/main/java/bambooSwords/RotateMatrix.java
@@ -0,0 +1,24 @@
+package bambooSwords;
+
+
+//Given input matrix = 
+//[
+//  [1,2,3],
+//  [4,5,6],
+//  [7,8,9]
+//],
+//
+//rotate the input matrix in-place such that it becomes:
+//[
+//  [7,4,1],
+//  [8,5,2],
+//  [9,6,3]
+//]
+
+public class RotateMatrix {
+	
+    public void rotate(int[][] matrix) {
+        
+    }
+
+}
